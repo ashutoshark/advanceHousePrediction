90aabd64554d5800f09b46f034c7f284f75dd715
ashutosh <ashutoshark08@gmail.com>
2023-01-29 01:32:34 +0530

    new

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import json
 import pickle
-form flask import Flask,requests,app,jsonify,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
  
 app=Flask(__name__)
 
-regmodle=pickle.load(open('regmodel.pkl','rb'))
-# scalar=
+regmodle=pickle.load(open('regmodle.pkl','rb'))
+#scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data=requests.json['data']
+    data=request.json['data']
     print(data)
-    print(np.array(list(data.values()))).reshape(1,-1)
-    new_data=
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=(np.array(list(data.values())).reshape(1,-1))
+    output=regmodle.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
