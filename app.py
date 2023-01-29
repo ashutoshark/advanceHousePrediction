0e3a51f94cba426f49480036df17a5c58832191f
ashutosh <ashutoshark08@gmail.com>
2023-01-29 22:34:08 +0530

    new

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import math 
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
@@ -7,7 +8,7 @@ import pandas as pd
 app=Flask(__name__)
 
 regmodle=pickle.load(open('regmodle.pkl','rb'))
-#scalar=pickle.load(open('scaling.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -22,5 +23,24 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=int(regmodle.predict(final_input)[0])
+    if(output<=1):
+       return render_template("home.html",prediction_text=" the flower Iris-Setosa  ".format(output))
+    elif(output>=1&output<=2):
+       return render_template("home.html",prediction_text=" the flower Iris-Versicolour  ".format(output))
+    else:
+        return render_template("home.html",prediction_text=" Iris-Virginica ".format(output))
+
+
 if __name__=="__main__":
     app.run(debug=True)
+   
+    
+   
+     
+
